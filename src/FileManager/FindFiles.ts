a1d99ad9153382746c15ef73bf2f3eb259819558
Max Sir <erotourtes@gmail.com>
2023-03-06 14:24:10 +0200

    refactor: moved findfiles into class

--- src/FileManager/FindFiles.ts
+++ src/FileManager/FindFiles.ts
@@ -1,12 +1,45 @@
 import fs from "node:fs";
+import path from "node:path";
 
-export default async function findFiles(...extensions: string[]) {
-  const files = await fs.promises.readdir(process.cwd(), {
-    withFileTypes: true,
-  });
-  console.log(files);
+export default class FindFiles {
+  private stack: string[] = [];
+  private foundFiles: string[] = [];
 
-  for (const file of files) {
-    console.log(file.name, file.isFile());
+  constructor(private extensions: string[]) {}
+
+  async find(rootPath: string) {
+    this.stack.push(rootPath);
+
+    while (this.stack.length) {
+      const rootPath = this.stack.pop()!;
+      await this.checkDir(rootPath);
+    }
+
+    return this.foundFiles;
+  }
+
+  private async checkDir(rootPath: string) {
+    const files = await fs.promises.readdir(rootPath, {
+      withFileTypes: true,
+    });
+
+    for (const file of files) {
+      if (this.isRigthFolder(file))
+        this.stack.push(path.join(rootPath, file.name));
+
+      if (this.isRightFile(file))
+        this.foundFiles.push(path.join(rootPath, file.name));
+    }
+  }
+
+  private isRigthFolder(file: fs.Dirent): boolean {
+    const isDotFolder = file.name.startsWith(".");
+    return file.isDirectory() && !isDotFolder;
+  }
+
+  private isRightFile(file: fs.Dirent) {
+    return (
+      file.isFile() && this.extensions.some((ext) => file.name.endsWith(ext))
+    );
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,9 @@
-import fs from "node:fs";
 import os from "node:os";
-import path from "node:path";
+import FindFiles from "./FileManager/FindFiles.js";
 
 const START_PATH = `${os.homedir()}`;
 
-const stack: string[] = [START_PATH];
-const foundFiles: string[] = [];
+const findFiles = new FindFiles(["pdf"]);
+const files = await findFiles.find(START_PATH);
 
-const extensions = ["pdf", "jpg"];
-
-const isRigthFolder = (file: fs.Dirent): boolean => {
-  const isDotFolder = file.name.startsWith(".");
-  return file.isDirectory() && !isDotFolder;
-};
-
-// const getFullLinkPath = async (
-//   file: fs.Dirent,
-//   rootPath: string
-// ): Promise<string> => {
-//   if (!file.isSymbolicLink()) return file.name;
-//
-//   const rightPath = await fs.promises.readlink(path.join(rootPath, file.name));
-//
-//   return rightPath;
-// };
-
-const isRightFile = (file: fs.Dirent) =>
-  file.isFile() && extensions.some((ext) => file.name.endsWith(ext));
-
-async function findFiles(rootPath: string) {
-  const files = await fs.promises.readdir(rootPath, {
-    withFileTypes: true,
-  });
-
-  for (const file of files) {
-    if (isRigthFolder(file)) stack.push(path.join(rootPath, file.name));
-
-    // if (file.isSymbolicLink()) {
-    //   const link = await getFullLinkPath(file, rootPath);
-    //   stack.push(path.join(rootPath, link));
-    // }
-
-    if (isRightFile(file)) {
-      foundFiles.push(path.join(rootPath, file.name));
-    }
-  }
-}
-
-async function find() {
-  while (stack.length) {
-    const rootPath = stack.pop()!;
-    await findFiles(rootPath);
-  }
-}
-
-await find();
-
-console.log(stack);
-console.log(foundFiles);
+console.log(files);
