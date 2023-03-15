8036b6169d6b2f1e4bb018769d5070cd161cb401
Max Sir <erotourtes@gmail.com>
2023-03-15 22:58:05 +0200

    fix: jest is now know about ts esmodules

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,18 @@
 // eslint-disable-next-line
 module.exports = {
-  // other configuration options...
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    ".*\\.test\\.ts$",
-  ],
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    "^.+\\.m?[tj]sx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+
+  testPathIgnorePatterns: ["/node_modules/", ".*\\.test\\.js$"],
 };
