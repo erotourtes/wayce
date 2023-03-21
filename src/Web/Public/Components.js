6e916aa6c2385d3779f5fe8020acf2ecc7f1ea36
Max Sir <erotourtes@gmail.com>
2023-03-21 11:16:42 +0200

    feat: frequency added

--- src/Web/Public/Components.js
+++ src/Web/Public/Components.js
@@ -1,13 +1,23 @@
 const ul = document.querySelector("ul");
 
+let mainFrequency = undefined;
+const relativeFrequency = (frequency) =>
+  (frequency / mainFrequency * 100).toFixed(2);
+
 export function addList(text, frequency, open) {
+  if (mainFrequency === undefined) {
+    mainFrequency = frequency;
+  }
+
   const li = document.createElement("li");
   li.innerText = text;
   li.setAttribute("path-to-file", text);
+  li.setAttribute("frequency", relativeFrequency(frequency));
   li.onclick = () => { open(text); };
   ul.appendChild(li);
 }
 
 export function removeAll() {
+  mainFrequency = undefined;
   ul.innerHTML = "";
 }
